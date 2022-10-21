334829e83b1fda8035df45f0a0ec9086e6266210
courage <couragesblog@gmail.com>
2022-10-21 12:01:42 +0100

    Test user sucessfully subscribed

--- php-stand-in-test/tests/SubscriptionTest.php
+++ php-stand-in-test/tests/SubscriptionTest.php
@@ -2,18 +2,31 @@
 
 namespace Tests;
 
+use App\Billing\PaymentGateway;
+use App\Models\User;
+use App\Subscription;
 use PHPUnit\Framework\TestCase;
 
-/**
- * SubScriptonTest
- * @group group
- */
-class SubScriptonTest extends TestCase
+
+class SubscriptionTest extends TestCase
 {
     /** @test */
-    public function test_function()
+    public function it_creates_stripe_subscription()
     {
         $this->markTestSkipped("we will come back top this ASAP");
     }
+
+    /** @test */
+    public function creating_a_subscription_marks_user_as_subscribed()
+    {
+        $user = new User();
+
+        $subscription = new Subscription(new PaymentGateway);
+
+        $subscription->create($user);
+
+        $this->assertTrue($user->isSubscribed());
+    }
+    
 
 }
